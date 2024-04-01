073818cb354d838c0c8189fbe687a8caa6d3143e
Benjamin Dehli <superelg@gmail.com>
2024-04-01 20:36:33 +0200

    Add components for settings menu

---
+++ src/components/Template/SettingsMenu.js
@@ -0,0 +1,54 @@
+// Dependencies
+import { Fragment, useState } from "react";
+
+// Material UI
+import { IconButton } from "@mui/material";
+import { Settings } from "@mui/icons-material";
+
+// Components
+import { StyledMenu } from "./StyledMenu";
+
+export function SettingsMenu({ elementItem, menuItems = [] }) {
+  const [settingsAnchorEl, setSettingsAnchorEl] = useState(null);
+  const settingsOpen = Boolean(settingsAnchorEl);
+  const handleSettingsClick = (event) => {
+    setSettingsAnchorEl(event.currentTarget);
+  };
+  const handleSettingsClose = () => {
+    setSettingsAnchorEl(null);
+  };
+  const menuId = `${elementItem?.id}-settings-menu`;
+  const menuButtonId = `${elementItem?.id}-settings-menu-button`;
+  return (
+    <Fragment>
+      <StyledMenu
+        id={menuId}
+        MenuListProps={{
+          "aria-labelledby": menuButtonId,
+        }}
+        anchorEl={settingsAnchorEl}
+        open={settingsOpen}
+        onClose={handleSettingsClose}
+      >
+        {menuItems?.props?.children?.map((menuItem, index) => {
+          return (
+            <div key={index} onClick={handleSettingsClose}>
+              {menuItem}
+            </div>
+          );
+        })}
+      </StyledMenu>
+      <IconButton
+        edge="end"
+        aria-label="comments"
+        id={menuButtonId}
+        aria-controls={settingsOpen ? menuId : undefined}
+        aria-haspopup="true"
+        aria-expanded={settingsOpen ? "true" : undefined}
+        onClick={handleSettingsClick}
+      >
+        <Settings />
+      </IconButton>
+    </Fragment>
+  );
+}
