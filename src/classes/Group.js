53a23df6a4491a6fea12f46cfd4232e970a1a5e0
Benjamin Dehli <superelg@gmail.com>
2024-04-07 21:55:21 +0200

    Add props to group and groups class

--- src/classes/Group.js
+++ src/classes/Group.js
@@ -3,7 +3,7 @@ import { Sample } from "./Sample";
 
 export class Group {
     constructor(props, effects, sample) {
-        this.id = props.id || uuidv4();
+        this.id = props?.id || uuidv4();
         this.elementType = "group";
         this.enabled = props?.enabled;
         this.tags = props?.tags;
@@ -12,8 +12,18 @@ export class Group {
         this.groupTuning = props?.groupTuning;
         this.volume = props?.volume;
         this.trigger = props?.trigger;
+        this.attack = props?.attack;
+        this.decay = props?.decay;
+        this.sustain = props?.sustain;
+        this.release = props?.release;
+        this.attackCurve = props?.attackCurve;
+        this.decayCurve = props?.decayCurve;
+        this.releaseCurve = props?.releaseCurve;
         this.effects = [];
-        this.samples = props.samples || sample?.map((sample) => new Sample({ ...sample.$, groupId: this.id }, sample.sample));
+        this.samples =
+            props?.samples ||
+            sample?.map((sample) => new Sample({ ...sample.$, groupId: this.id }, sample.sample)) ||
+            [];
     }
     toJson() {
         return {
@@ -24,13 +34,20 @@ export class Group {
                 ampVelTrack: this.ampVelTrack,
                 groupTuning: this.groupTuning,
                 volume: this.volume,
-                trigger: this.trigger
+                trigger: this.trigger,
+                attack: this.attack,
+                decay: this.decay,
+                sustain: this.sustain,
+                release: this.release,
+                attackCurve: this.attackCurve,
+                decayCurve: this.decayCurve,
+                releaseCurve: this.releaseCurve
             },
             sample: this.samples?.map((sample) => sample.toJson())
         };
     }
     newSample() {
-        this.samples.push(new Sample({}));
+        this.samples.push(new Sample());
     }
     addSample(sample) {
         this.samples.push(sample);
--- src/classes/Groups.js
+++ src/classes/Groups.js
@@ -3,28 +3,29 @@ import { Group } from "./Group";
 
 export class Groups {
     constructor(props, groups) {
-        this.id = uuidv4();
+        this.id = props?.id || uuidv4();
         this.elementType = "groups";
-        this.attack = props.attack;
-        this.attackCurve = props.attackCurve;
-        this.decay = props.decay;
-        this.decayCurve = props.decayCurve;
-        this.sustain = props.sustain;
-        this.release = props.release;
-        this.releaseCurve = props.releaseCurve;
-        this.volume = props.volume;
-        this.ampVelTrack = props.ampVelTrack;
-        this.groups = groups.map((group) => new Group({ ...group.$ }, groups.effects, group.sample));
+        this.attack = props?.attack;
+        this.decay = props?.decay;
+        this.sustain = props?.sustain;
+        this.release = props?.release;
+        this.attackCurve = props?.attackCurve;
+        this.decayCurve = props?.decayCurve;
+        this.releaseCurve = props?.releaseCurve;
+        this.volume = props?.volume;
+        this.ampVelTrack = props?.ampVelTrack;
+        this.groups =
+            props?.groups || groups?.map((group) => new Group({ ...group.$ }, groups.effects, group.sample)) || [];
     }
     toJson() {
         return {
             $: {
                 attack: this.attack,
-                attackCurve: this.attackCurve,
                 decay: this.decay,
-                decayCurve: this.decayCurve,
                 sustain: this.sustain,
                 release: this.release,
+                attackCurve: this.attackCurve,
+                decayCurve: this.decayCurve,
                 releaseCurve: this.releaseCurve,
                 volume: this.volume,
                 ampVelTrack: this.ampVelTrack
@@ -33,7 +34,7 @@ export class Groups {
         };
     }
     newGroup() {
-        this.groups.push(new Group({}));
+        this.groups.push(new Group());
     }
     addGroup(group) {
         this.groups.push(group);
