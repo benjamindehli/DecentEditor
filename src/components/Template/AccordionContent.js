1b3eec1365891b2aac7f7d161277b1f4d4462b87
Benjamin Dehli <superelg@gmail.com>
2024-04-09 22:11:04 +0200

    Remove unused components

--- src/components/Template/AccordionContent.js
+++
@@ -1,6 +0,0 @@
-// Stylesheets
-import style from "./AccordionContent.module.scss";
-
-export function AccordionContent({ children }) {
-    return <div className={style.accordionContent}>{children}</div>;
-}
