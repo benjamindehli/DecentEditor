507b61dfc230baffd6f12460cc1b3918a75b8b14
Benjamin Dehli <superelg@gmail.com>
2024-04-29 23:19:07 +0200

    Remove unused stylesheet import

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -6,9 +6,6 @@ import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
 import { DecentSamplerContextProvider } from "@/store/DecentSamplerContext";
 import { ColorModeContextProvider } from "@/store/ColorModeContext";
 
-// Stylesheets
-import "@/app/globals.css";
-
 export const metadata = {
     title: "DecentEditor",
     description: "Editor for DecentSampler preset files"
