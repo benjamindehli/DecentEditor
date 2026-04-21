9fd2c0e32c5714c9af7f12b6dfc62c481030bd7b
Benjamin Dehli <superelg@gmail.com>
2026-04-21 21:29:37 +0200

    Update LabelItemComponent to support item removal and coordinate display
    Implement functional item removal and update the UI to display label text within a Chip. This also adds x/y coordinate information to the secondary text area and removes unused expansion logic for child elements.

--- src/components/DecentSampler/Label/LabelItemComponent.jsx
+++ src/components/DecentSampler/Label/LabelItemComponent.jsx
@@ -1,17 +1,14 @@
 // Dependencies
-import { Fragment, useContext, useState } from "react";
+import { Fragment } from "react";
 
 // Material UI
 import MenuItem from "@mui/material/MenuItem";
-import { Chip, Collapse, List, ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
-import { Abc, ChevronRight, ExpandMore, Folder } from "@mui/icons-material";
+import { Chip, ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
+import { Abc } from "@mui/icons-material";
 import { useTheme } from "@mui/material/styles";
 
-// Components
-import { BindingItemComponent } from "../Binding/BindingItemComponent";
-
 // Template
-import { IconAdd } from "@/components/Template/Icons/IconAdd";
+import { IconRemove } from "@/components/Template/Icons/IconRemove";
 import { ListItemSecondaryText } from "@/components/Template/ListItemSecondaryText";
 import { DefaultListItem } from "@/components/Template/DefaultListItem";
 
@@ -19,72 +16,37 @@ import { DefaultListItem } from "@/components/Template/DefaultListItem";
 import { getIndentSize } from "@/functions/helpers";
 import { getColorForElementType } from "@/functions/styles";
 
-// Store
-import DecentSamplerContext from "@/store/DecentSamplerContext";
-
-export function LabelItemComponent({ labelItem }) {
+export function LabelItemComponent({ labelItem, onRemoveItem }) {
     const theme = useTheme();
-
-    const decentSamplerContext = useContext(DecentSamplerContext);
-
-    const [isExpanded, setIsExpanded] = useState(false);
 
     const settingsMenuItems = (
         <Fragment>
-            <MenuItem
-                onClick={() => {
-                    // keyboardItem.newColor();
-                    // decentSamplerContext.updateKeyboardItem(keyboardItem);
-                }}
-                disableRipple
-            >
-                <IconAdd>
-                    <Folder />
-                </IconAdd>
-                Add color
-            </MenuItem>
-            <MenuItem
-                onClick={() => {
-                    //   keyboardItem.newColor();
-                    //   decentSamplerContext.updateKeyboardItem(keyboardItem);
-                }}
-                disableRipple
-            >
-                <Folder />
-                Add multiple colors
-            </MenuItem>
+            {onRemoveItem && (
+                <MenuItem onClick={() => onRemoveItem(labelItem.id)} disableRipple>
+                    <IconRemove><Abc /></IconRemove>
+                    Remove label
+                </MenuItem>
+            )}
         </Fragment>
     );
 
-    function hasChildren() {
-        return !!labelItem?.childElements?.length;
-    }
+    const textChip = labelItem?.text?.length
+        ? <Chip component="span" label={labelItem.text} size="small" />
+        : null;
 
-    const primaryText = <Fragment>Label</Fragment>;
-
-    const secondaryText = <ListItemSecondaryText>{!!labelItem?.text?.length && labelItem.text}</ListItemSecondaryText>;
-
-    function renderChildElement(childElement) {
-        switch (childElement?.elementType) {
-            case "binding":
-                return <BindingItemComponent key={childElement.id} bindingItem={childElement} />;
-            default:
-                return null;
-        }
-    }
+    const primaryText = <Fragment>Label {textChip}</Fragment>;
+    const secondaryText = labelItem?.x !== undefined && labelItem?.y !== undefined
+        ? <ListItemSecondaryText>x: {labelItem.x}, y: {labelItem.y}</ListItemSecondaryText>
+        : null;
 
     return (
         <Fragment>
             <DefaultListItem
                 elementItem={labelItem}
                 settingsMenuItems={settingsMenuItems}
-                onEditButtonClick={() => console.log("onClick")}
+
             >
-                <ListItemButton
-                    sx={{ pl: getIndentSize(labelItem, hasChildren()) }}
-                    onClick={() => setIsExpanded(!isExpanded)}
-                >
-                    {hasChildren() ? isExpanded ? <ExpandMore /> : <ChevronRight /> : null}
+                <ListItemButton sx={{ pl: getIndentSize(labelItem, false) }}>
                     <ListItemIcon
                         sx={{
                             minWidth: "32px",
@@ -96,14 +58,6 @@ export function LabelItemComponent({ labelItem }) {
                     <ListItemText primary={primaryText} secondary={secondaryText} />
                 </ListItemButton>
             </DefaultListItem>
-            {hasChildren() && (
-                <Collapse in={isExpanded} timeout="auto" unmountOnExit>
-                    <List dense component="div" disablePadding>
-                        {!!labelItem?.childElements?.length &&
-                            labelItem.childElements.map((childElement) => renderChildElement(childElement))}
-                    </List>
-                </Collapse>
-            )}
         </Fragment>
     );
 }
